a100239dd612cee47282b0e97fcedaa69013981f
Rihards Jozēns <arbalets@gmail.com>
2019-09-24 14:18:46 +0300

    iOS 13 statubar coloring fix

--- XF.Material/XF.Material.iOS/Utility/MaterialUtility.cs
+++ XF.Material/XF.Material.iOS/Utility/MaterialUtility.cs
@@ -18,7 +18,21 @@ namespace XF.Material.iOS.Utility
         {
             var isColorDark = color.ToCGColor().IsColorDark();
             UIApplication.SharedApplication.StatusBarStyle = isColorDark ? UIStatusBarStyle.LightContent : UIStatusBarStyle.Default;
-            if (UIApplication.SharedApplication.ValueForKey(new NSString("statusBar")) is UIView statusBar) statusBar.BackgroundColor = color.ToUIColor();
+
+            if (UIDevice.CurrentDevice.CheckSystemVersion(13, 0))
+            {
+                // TODO: This is a temp solution, see https://stackoverflow.com/questions/58069085/change-status-bar-colour-on-ios13
+                var statusBar = new UIView(UIApplication.SharedApplication.StatusBarFrame)
+                {
+                    BackgroundColor = color.ToUIColor()
+                };
+                UIApplication.SharedApplication.Windows[0].AddSubview(statusBar);
+            }
+            else
+            {
+                if (UIApplication.SharedApplication.ValueForKey(new NSString("statusBar")) is UIView statusBar)
+                    statusBar.BackgroundColor = color.ToUIColor();
+            }
         }
     }
 }
